87e37ba458792ca47caf4ae7121d4227121e1115
Irpe1337master <iiro.roar@gmail.com>
2022-01-08 09:42:36 +0200

    Fix: add "node" to path so windows accepts dot.

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,7 @@ module.exports = function(grunt) {
 		pkg: grunt.file.readJSON("package.json"),
 		eslint: {
 			options: {
-				configFile: ".eslintrc.json"
+				overrideConfigFile: ".eslintrc.json"
 			},
 			target: ["*.js"]
 		},
